88cf3b31e64138f32c15947d8775f82244d2e497
sunnybeatsza <makgerutumisho55@gmail.com>
2024-08-29 15:15:54 +0200

    Added Navbar - No functionality implemented yet. Also added assets, dependencies and etc.

---
+++ nokami_frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+import LogoImg from "../../Assets/Logo1_Copy.jpg";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar-items py-3">
+      <a href="/">
+        <img src={LogoImg} alt="Logo-Img" id="Logo-Img" />
+      </a>
+      <ul>
+        <li>HOME</li>
+        <li>PRODUCTS</li>
+        <li>ABOUT</li>
+        <li>CONTACT</li>
+      </ul>
+      <div className="nav-icons">
+        <i class="bi bi-search"></i>
+        <i class="bi bi-person"></i>
+        <i class="bi bi-bag"></i>
+      </div>
+    </div>
+  );
+};
---
+++ nokami_frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import { Navbar } from "../../Components/Navbar/Navbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
